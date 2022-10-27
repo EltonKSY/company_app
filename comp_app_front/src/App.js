eacc175316c71b0b178c52adf7cf98900476cb7c
Elton360Kaseya <elton.lucien@kaseya.com>
2022-10-27 12:07:21 -0400

    ui implemented
    UI implemented with basic validation for username and password lengths.

--- comp_app_front/src/App.js
+++ comp_app_front/src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AuthPage from "./pages/AuthPage";
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
