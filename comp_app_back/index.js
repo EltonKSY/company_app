224b658d0674df2da059e7a4950f93553cbeec84
Elton360Kaseya <elton.lucien@kaseya.com>
2022-11-04 17:10:43 -0400

    Backend Init

--- comp_app_back/index.js
+++ comp_app_back/index.js
@@ -1,5 +1,52 @@
 const express = require("express");
+const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const connection = mysql.createConnection({
+  host: "autoconfig.gourmandmiami.com",
+  user: "clubst5_comp_admin",
+  password: "Plane.4580skip",
+  database: "clubst5_company",
+});
+
+app.post("/Authenticate", async (req, res) => {
+  const q = `SELECT * FROM Users WHERE user_name = "${req.body.user_name}"`;
+
+  connection.query(q, [], async function (err, user) {
+    if (!user?.length)
+      return res.status(400).send("Authentication failed, invalid credentials");
+
+    connection.end();
+
+    try {
+      if (await bcrypt.compare(req?.body?.password, user[0].pw))
+        res.status(200).send("Authentication succeeded");
+      else {
+        res.status(403).send("Authentication failed, invalid credentials");
+      }
+    } catch {
+      res.status(500).send();
+    }
+  });
+});
+
+app.get("/Employees", async (req, res) => {
+  const q = "SELECT * FROM Employees";
+
+  connection.query(q, [], function (err, result) {
+    console.log(result);
+    return res.status(200).json({
+      status: "success",
+      result,
+    });
+  });
+
+  connection.end();
+});
 
 app.listen(3001, "localhost", () => console.log("Running on port 3001!"));
